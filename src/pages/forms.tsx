92d1efd018cc18a4c2f4f98d00f21b4c56295da1
nawhgnues <seungdang123jp@gmail.com>
2023-05-31 23:59:43 +0900

    feat: making forms alone

---
+++ src/pages/forms.tsx
@@ -0,0 +1,75 @@
+/**
+ *  There are many things to consider when making a form.
+ *  Check whether the data is empty and validate its formatting, among other things.
+ *
+ *  Making a form is much more tedious than one might think.
+ *  -> Try to use react-hook-form
+ */
+
+import { useState } from "react";
+
+export default function Forms() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [formErrors, setFormErrors] = useState("");
+  const [emailErrors, setEmailErrors] = useState("");
+
+  const onUsernameChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setUsername(value);
+  };
+
+  const onEmailChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setEmail(value);
+  };
+
+  const onPasswordChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setPassword(value);
+  };
+
+  const onSubmit = (event: React.SyntheticEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (username === "" || email === "" || password === "") {
+      setFormErrors("All fields are required");
+    }
+    if (!email.includes("@")) {
+      setEmailErrors("balalal");
+    }
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        value={username}
+        onChange={onUsernameChange}
+        type="text"
+        placeholder="Username"
+        required
+        minLength={5}
+      />
+      <input
+        value={email}
+        onChange={onEmailChange}
+        type="email"
+        placeholder="Email"
+        required
+      />
+      <input
+        value={password}
+        onChange={onPasswordChange}
+        type="password"
+        placeholder="Password"
+        required
+      />
+      <input type="submit" value={"Create Account"} />
+    </form>
+  );
+}
