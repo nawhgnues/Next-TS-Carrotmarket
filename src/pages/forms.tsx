4346dc5ba206847e7dd414211de5f6c5dec7f89e
nawhgnues <seungdang123jp@gmail.com>
2023-06-02 19:08:20 +0900

    feat: validation part two

--- src/pages/forms.tsx
+++ src/pages/forms.tsx
@@ -1,51 +1,47 @@
-import { useForm } from "react-hook-form";
+import { FieldErrors, useForm } from "react-hook-form";
 
-// Less code (c)
 // Better validation
-// Better errors (set, clear, display)
+// Better Erros (set, clear, display)
 // Have control over inputs
-// Dont deal with events (c)
-// Easier Inputs (c)
+
+interface LoginForm {
+  username: string;
+  password: string;
+  email: string;
+}
 
 export default function Forms() {
-  const { register, handleSubmit } = useForm();
-  const onValid = () => {
-    console.log("i'm valid bby");
+  const { register, handleSubmit } = useForm<LoginForm>();
+  const onValid = (data: LoginForm) => {
+    console.log("im valid bby");
+  };
+  const onInvalid = (errors: FieldErrors) => {
+    console.log(errors);
   };
   return (
-    <form onSubmit={handleSubmit(onValid)}>
+    <form onSubmit={handleSubmit(onValid, onInvalid)}>
       <input
         {...register("username", {
-          required: true,
+          required: "Username is required",
+          minLength: {
+            message: "The username should be longer than 5 chars.",
+            value: 5,
+          },
         })}
         type="text"
         placeholder="Username"
-        minLength={5}
       />
       <input
-        {...(register("email"),
-        {
-          required: true,
-        })}
+        {...register("email", { required: "Email is required" })}
         type="email"
         placeholder="Email"
       />
       <input
-        {...(register("password"),
-        {
-          required: true,
-        })}
+        {...register("password", { required: "Password is required" })}
         type="password"
         placeholder="Password"
       />
-      <input
-        {...(register("submit"),
-        {
-          required: true,
-        })}
-        type="submit"
-        value={"Create Account"}
-      />
+      <input type="submit" value="Create Account" />
     </form>
   );
 }
