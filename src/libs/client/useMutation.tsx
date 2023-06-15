2bea43fdfe2a4cf99cdde50edf0ca8cc1cd39e10
nawhgnues <seungdang123jp@gmail.com>
2023-06-16 00:03:37 +0900

    fix: Clean Code part Two comment

--- src/libs/client/useMutation.tsx
+++ src/libs/client/useMutation.tsx
@@ -6,6 +6,13 @@ export default function useMutation(
   (data: any) => void,
   { loading: boolean; data: undefined | any; error: undefined | any }
 ] {
+  // useState를 여러번 사용하는 것보다 object를 이용해서 상태관리 하는것이 더 깔끔할듯
+  // const [state, setState] = useState({
+  //   loading: false,
+  //   data: undefined,
+  //   error: undefined,
+  // });
+
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState<undefined | any>(undefined);
   const [error, setError] = useState<undefined | any>(undefined);
