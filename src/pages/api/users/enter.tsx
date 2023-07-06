c39cc2af93956b50467ec566f05824df3fb7f4fa
nawhgnues <seungdang123jp@gmail.com>
2023-07-06 23:45:34 +0900

    feat: Sending Email

--- src/pages/api/users/enter.tsx
+++ src/pages/api/users/enter.tsx
@@ -1,7 +1,10 @@
+import mail from "@sendgrid/mail";
 import twilio from "twilio";
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "@/src/libs/server/withHandler";
 import client from "@/src/libs/server/client";
+
+mail.setApiKey(process.env.SENDGRID_KEY!);
 
 const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
@@ -35,6 +38,14 @@ async function handler(
       from: "+12176463348",
       to: process.env.PHONE_NUMBER!,
       body: `Your login token is ${payload}.`,
+    });
+  } else if (email) {
+    const email = await mail.send({
+      from: process.env.MY_EMAIL!,
+      to: process.env.MY_EMAIL!,
+      subject: "Your Carrot Market Verification Email",
+      text: `Your token is ${payload}`,
+      html: `<strong>Your token is ${payload}</strong>`,
     });
   }
   return res.json({
