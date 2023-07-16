c80a48777c2d7e132914f009d5e2bed036ed4d45
nawhgnues <seungdang123jp@gmail.com>
2023-07-16 23:14:09 +0900

    fix: useUser Refactor

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,20 @@
-import "@/src/styles/globals.css";
+import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { SWRConfig } from "swr";
 
-export default function App({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="w-full max-w-lg mx-auto">
-      <Component {...pageProps} />
-    </div>
+    <SWRConfig
+      value={{
+        fetcher: (url: string) =>
+          fetch(url).then((response) => response.json()),
+      }}
+    >
+      <div className="w-full max-w-xl mx-auto">
+        <Component {...pageProps} />
+      </div>
+    </SWRConfig>
   );
 }
+
+export default MyApp;
